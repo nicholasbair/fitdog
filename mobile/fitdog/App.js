3f3bb122cba7f7168f5b1f1f0a1d3ea241723143
Nick Bair <nickbair@golfingbuddha.com>
2017-06-04 18:17:30 -0400

    Remove Navigator (depricated), add Navigation, successfully render signin screen.

--- mobile/fitdog/App.js
+++ mobile/fitdog/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-export default class App extends React.Component {
+import Signin from './src/Components/Auth/Signin';
+// import Signup from './src/Components/Auth/Signup';
+// import ActivityDetail from './src/Components/Main/ActivityDetail';
+// import AddDog from './src/Components/Main/AddDog';
+// import Dashboard from './src/Components/Main/Dashboard';
+// import DogDetail from './src/Components/Main/DogDetail';
+// import LogActivity from './src/Components/Main/LogActivity';
+// import Account from './src/Components/Account/Account';
+
+import reducers from './src/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const SimpleApp = StackNavigator ({
+  Signin: { screen: Signin }
+});
+
+
+// {/* <Provider store={store}> */}
+// </Provider>
+
+
+export default class App extends Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Signin />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
